85c33118cb53e5d8e6a89926091a04c93dd518ae
magiccoder890 <23fzhu@athenian.org>
2020-09-30 14:04:01 +0000

    This is Bert's homework

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,25 +1,58 @@
 public class MyMain {
 
-    // Reverses an array
+   
     public static int[] reverse(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int[] new_arr = new int[arr.length];
+        int num = 0;
+        for (int i = arr.length; i > 0; i--){
+            new_arr[num] = arr[i-1];
+            num ++;
+        }
+        for (int i = 0;i < arr.length;i ++){
+            System.out.print(new_arr[i]);
+        }
+        return new_arr;
     }
 
-    // Finds the second largest number in an array
     public static int secondLargest(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        int max = -10000;
+        for (int i = arr.length; i > 1; i--){
+            if (Math.max(arr[i-1],arr[i-2]) > max ) {
+                max = Math.max(arr[i-1], arr[i - 2]);
+            }
+        }
+        for (int n = arr.length; n >0; n--){
+            if (arr[n-1] == max) {
+                arr[n-1] = -10000;
+            }
+        }
+        int max_final = -10000;
+        for (int i = arr.length; i > 1; i--){
+            if (Math.max(arr[i-1],arr[i-2]) > max_final ) {
+                max_final = Math.max(arr[i-1], arr[i - 2]);
+            }
+        }
+        System.out.print(max_final);
+        return max_final;
     }
 
-    // Checks to see if an array contains a geometric series
     public static boolean isGeometric(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return false;
+        double answer = 0;
+        boolean answer_1 = true;
+        double check = (double)arr[arr.length-1]/arr[arr.length-2];
+        for (int i = arr.length; i > 1; i--) {
+            answer = (double)arr[i-1]/arr[i-2];
+            if (answer ==  check){
+                answer_1 = true;
+            }
+            else {
+                answer_1 = false;
+                break;
+            }
+        }
+        return answer_1;
     }
-
-
-    public static void main(String[] args) {
-        // YOUR CODE HERE
+    public static void main(String arg[]){
+        System.out.print("All should work");
     }
 }
